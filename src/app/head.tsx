da14cf37ee524e06c0af2b927fec83364002d518
brenno <brennoaraujoqueiroz@gmail.com>
2023-10-14 02:55:41 -0300

    feat: add favicon

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,6 @@ export default function Head() {
       <title>Camada</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Launch your project web3" />
-      <link rel="icon" href="/images/favicon.ico" />
     </>
   )
 }
