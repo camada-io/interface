1c74cfb6cabce6c316e68c571b84c6602e260a51
brenno <brennoaraujoqueiroz@gmail.com>
2023-10-23 11:22:12 -0300

    feat: implements about section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { About } from '@/components/About'
 import { Apply } from '@/components/Apply'
 import { Hero } from '@/components/Hero'
 import { Stake } from '@/components/Stake'
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <Apply />
       <Stake />
+      <About />
       <Syslabs />
     </div>
   )
