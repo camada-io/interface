3b743613fc1d15d574a8c6e52f8e72b245fe6330
brenno <brennoaraujoqueiroz@gmail.com>
2023-10-30 22:49:52 -0300

    feat: create page with route and header page

---
+++ src/app/apply/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { PageHeader } from "@/components/PageHeader"
+
+export default function Apply() {
+  return (
+    <>
+      <PageHeader
+        title={"Apply your project"}
+        description={
+          "Apply through this form to bring your project onto the Camada Platform and into the Syscoin Ecosystem."
+        }
+      />
+    </>
+  )
+}
