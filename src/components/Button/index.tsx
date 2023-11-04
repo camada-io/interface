d143ebeab2e440ef4ccb2e8a72566c1899a545ae
brenno <brennoaraujoqueiroz@gmail.com>
2023-11-04 18:12:34 -0300

    chore: change some components to adapt on forms

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,29 +1,37 @@
+import { useMemo } from "react"
 import { Loading } from "../Loading"
 
 interface Props {
   maxWidth?: string
   height?: string
   isLoading?: boolean
+  disabled?: boolean
   icon?: JSX.Element
-  onClick: () => void
+  onClick?: () => void
   text: string
+  type?: "button" | "reset" | "submit" | undefined
 }
 
 export const Button = ({
   maxWidth,
   height = "h-[45px]",
   isLoading = false,
+  disabled = false,
   icon,
   onClick,
   text,
+  type = "button",
 }: Props) => {
+  const disable = useMemo(() => disabled || isLoading, [disabled, isLoading])
+
   return (
     <button
+      type={type}
       className={`${maxWidth} w-full ${height} px-6 py-4 bg-brandBlue-200 rounded-[5px] flex justify-center items-center gap-2.5 ${
-        isLoading ? "opacity-50 cursor-not-allowed" : ""
+        disable ? "opacity-50 cursor-not-allowed" : ""
       }`}
       onClick={onClick}
-      disabled={isLoading}
+      disabled={disabled || isLoading}
     >
       {isLoading ? (
         <Loading size={22} />
