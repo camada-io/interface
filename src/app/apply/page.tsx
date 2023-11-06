f42414a2a35dbf21515110e19dec7f85d7d9b293
brenno <brennoaraujoqueiroz@gmail.com>
2023-11-06 19:17:01 -0300

    feat: Implements Accordion component on Apply page

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -6,6 +6,7 @@ import { FormTwo } from "./forms/formTwo"
 import { FormThree } from "./forms/formThree"
 import { FormFour } from "./forms/formFour"
 import { FormFive } from "./forms/formFive"
+import { Accordion } from "@/components/Accordion"
 
 export default function Apply() {
   return (
@@ -19,131 +20,76 @@ export default function Apply() {
 
       <div className=" p-[32.5px] lg:p-32">
         <div className="w-full h-full flex-col justify-start items-end gap-6 inline-flex">
-          <div className="max-w-[1240px] w-full justify-start items-center gap-6 inline-flex">
-            <div className="h-[45px] w-[45px] pt-2 pb-[7px] bg-gray-600 rounded-[5px] border border-brandBlue-100 justify-center items-center flex">
-              <div className="text-brandBlue-100 text-xl font-bold leading-[30px]">
-                1
+          <Accordion number={1} title={"Personal info"}>
+            <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
+              <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
+                Personal information and representatives or those responsible
+                for the project.
               </div>
+              <FormOne
+                defaultValues={{
+                  name: "",
+                  email: "",
+                  nameProject: "",
+                  emailProject: "",
+                  agreedTerms: false,
+                }}
+                onSubmit={() => null}
+              />
             </div>
-            <div className="text-white text-xl font-extrabold leading-[30px]">
-              Personal info
-            </div>
-            <div className="grow shrink basis-0 h-4 py-2 justify-between items-center flex">
-              <div className="grow shrink basis-0 h-[0px] border border-white border-opacity-20"></div>
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
-            <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
-              Personal information and representatives or those responsible for
-              the project.
-            </div>
-            <FormOne
-              defaultValues={{
-                name: "",
-                email: "",
-                nameProject: "",
-                emailProject: "",
-                agreedTerms: false,
-              }}
-              onSubmit={() => null}
-            />
-          </div>
-          <div className="max-w-[1240px] w-full justify-start items-center gap-6 inline-flex">
-            <div className="h-[45px] w-[45px] pt-2 pb-[7px] bg-gray-600 rounded-[5px] border border-brandBlue-100 justify-center items-center flex">
-              <div className="text-brandBlue-100 text-xl font-bold leading-[30px]">
-                2
+          </Accordion>
+          <Accordion number={2} title={"Project info"}>
+            <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
+              <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
+                Let us know about your project idea and what you expect.
               </div>
+              <FormTwo
+                defaultValues={{
+                  projectName: "",
+                  about: "",
+                }}
+                onSubmit={() => null}
+              />
             </div>
-            <div className="text-white text-xl font-extrabold leading-[30px]">
-              Project info
-            </div>
-            <div className="grow shrink basis-0 h-4 py-2 justify-between items-center flex">
-              <div className="grow shrink basis-0 h-[0px] border border-white border-opacity-20"></div>
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
-            <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
-              Let us know about your project idea and what you expect.
-            </div>
-            <FormTwo
-              defaultValues={{
-                projectName: "",
-                about: "",
-              }}
-              onSubmit={() => null}
-            />
-          </div>
-          <div className="max-w-[1240px] w-full justify-start items-center gap-6 inline-flex">
-            <div className="h-[45px] w-[45px] pt-2 pb-[7px] bg-gray-600 rounded-[5px] border border-brandBlue-100 justify-center items-center flex">
-              <div className="text-brandBlue-100 text-xl font-bold leading-[30px]">
-                3
+          </Accordion>
+          <Accordion number={3} title={"House of Chimera report"}>
+            <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
+              <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
+                Our Partner House of Chimera will write a research report about
+                your project. This includes details about your team and your
+                personal history.
               </div>
+              <FormThree
+                defaultValues={{
+                  agreedResearch: false,
+                }}
+                onSubmit={() => null}
+              />
             </div>
-            <div className="text-white text-xl font-extrabold leading-[30px]">
-              House of Chimera report
-            </div>
-            <div className="grow shrink basis-0 h-4 py-2 justify-between items-center flex">
-              <div className="grow shrink basis-0 h-[0px] border border-white border-opacity-20"></div>
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
-            <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
-              Our Partner House of Chimera will write a research report about
-              your project. This includes details about your team and your
-              personal history.
-            </div>
-            <FormThree
-              defaultValues={{
-                agreedResearch: false,
-              }}
-              onSubmit={() => null}
-            />
-          </div>
-          <div className="max-w-[1240px] w-full justify-start items-center gap-6 inline-flex">
-            <div className="h-[45px] w-[45px] pt-2 pb-[7px] bg-gray-600 rounded-[5px] border border-brandBlue-100 justify-center items-center flex">
-              <div className="text-brandBlue-100 text-xl font-bold leading-[30px]">
-                4
+          </Accordion>
+          <Accordion number={4} title={"Upfront costs"}>
+            <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
+              <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
+                Costs associated with launching on Camada.
               </div>
+              <FormFour
+                defaultValues={{
+                  agreedUpfront: false,
+                }}
+                onSubmit={() => null}
+              />
             </div>
-            <div className="text-white text-xl font-extrabold leading-[30px]">
-              Upfront costs
-            </div>
-            <div className="grow shrink basis-0 h-4 py-2 justify-between items-center flex">
-              <div className="grow shrink basis-0 h-[0px] border border-white border-opacity-20"></div>
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
-            <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
-              Costs associated with launching on Camada.
-            </div>
-            <FormFour
-              defaultValues={{
-                agreedUpfront: false,
-              }}
-              onSubmit={() => null}
-            />
-          </div>
-          <div className="max-w-[1240px] w-full justify-start items-center gap-6 inline-flex">
-            <div className="h-[45px] w-[45px] pt-2 pb-[7px] bg-gray-600 rounded-[5px] border border-brandBlue-100 justify-center items-center flex">
-              <div className="text-brandBlue-100 text-xl font-bold leading-[30px]">
-                5
+          </Accordion>
+          <Accordion number={5} title={"Optional feedback"}>
+            <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
+              <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
+                Thank you for choosing to work with us. We strive to be the most
+                trustworthy launchpad in the crypto ecosystem and are always
+                trying to improve.
               </div>
+              <FormFive defaultValues={{}} onSubmit={() => null} />
             </div>
-            <div className="text-white text-xl font-extrabold leading-[30px]">
-              Optional feedback
-            </div>
-            <div className="grow shrink basis-0 h-4 py-2 justify-between items-center flex">
-              <div className="grow shrink basis-0 h-[0px] border border-white border-opacity-20"></div>
-            </div>
-          </div>
-          <div className="flex flex-col lg:flex-row w-full lg:pl-[70px] justify-center items-start gap-6 ">
-            <div className="lg:max-w-[246px] w-full grow shrink basis-0 text-white text-base font-normal leading-relaxed">
-              Thank you for choosing to work with us. We strive to be the most
-              trustworthy launchpad in the crypto ecosystem and are always
-              trying to improve.
-            </div>
-            <FormFive defaultValues={{}} onSubmit={() => null} />
-          </div>
+          </Accordion>
 
           <div className="w-full h-full pt-6 justify-start items-center gap-2.5 inline-flex">
             <div className="flex flex-col grow shrink basis-0 text-center">
---
+++ src/components/Accordion/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+type Props = {
+  number: number
+  title: string
+  children: any
+}
+
+export const Accordion = ({ number, title, children }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <>
+      <button
+        className="flex items-center w-full text-left focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className="max-w-[1240px] w-full justify-start items-center gap-6 inline-flex">
+          <div className="h-[45px] w-[45px] pt-2 pb-[7px] bg-gray-600 rounded-[5px] border border-brandBlue-100 justify-center items-center flex">
+            <div className="text-brandBlue-100 text-xl font-bold leading-[30px]">
+              {number}
+            </div>
+          </div>
+          <div className="text-white text-xl font-extrabold leading-[30px]">
+            {title}
+          </div>
+          <div className="grow shrink basis-0 h-4 py-2 justify-between items-center flex">
+            <div className="grow shrink basis-0 h-[0px] border border-white border-opacity-20"></div>
+          </div>
+        </div>
+      </button>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } overflow-hidden transition-height duration-300 ease-in-out w-full h-full`}
+      >
+        {children}
+      </div>
+    </>
+  )
+}
