797516fafd5ef9d78f659564208278fc01554384
Ricardo Vieira <hi@ricvieira.com>
2024-02-09 09:41:49 -0300

    fix build error

--- src/utils/attest.ts
+++ src/utils/attest.ts
@@ -5,17 +5,21 @@ const easContractAddress = process.env.NEXT_PUBLIC_EAS_ADDRESS as string
 const schemaUID = process.env.NEXT_PUBLIC_SCHEMA_UID as string
 const privateKey = process.env.NEXT_PUBLIC_PK as string
 const env = process.env.NEXT_PUBLIC_APP_ENV as "development" | "production"
-const eas = new EAS(easContractAddress)
+
 const rpc = {
   development: "https://rpc-tanenbaum.rollux.com",
   production: "https://rpc.rollux.com",
 }
 
-const provider = new JsonRpcProvider(rpc[env])
-const signer = new Wallet(privateKey, provider)
-
 export async function attest(address: string) {
-  await eas.connect(signer as any)
+  if ([easContractAddress, schemaUID, privateKey, env].some((env) => !env))
+    throw new Error("Missing environment variables")
+
+  const provider = new JsonRpcProvider(rpc[env])
+  const signer = new Wallet(privateKey, provider)
+  const eas = new EAS(easContractAddress)
+
+  eas.connect(signer as any)
 
   const schemaEncoder = new SchemaEncoder("bool whitelist")
 
